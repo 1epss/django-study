bed7b2a6eda7c8a2d4dd05495f0c2501d3456074
Silent gyuu <110515873+1epss@users.noreply.github.com>
2022-10-18 15:31:38 +0900

    삭제된 작성자는 None으로 표시하기

--- blog/models.py
+++ blog/models.py
@@ -15,7 +15,7 @@ class Post(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)     # auto_now_add = True 는 레코드가 생성될 때 현재 시각이 자동으로 저장
     updated_at = models.DateTimeField(auto_now=True)         # auto_now = True 는 다시 저장할 때 마다 그 시각이 저장
 
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
 
     def __str__(self):
         return f'[{self.pk}]{self.title} :: {self.author}'        # 해당 포스트의 pk 값, 해당 포스트의 title 값
