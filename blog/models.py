84d8435af6f98acb468faec3cf03280414b803ac
Silent gyuu <110515873+1epss@users.noreply.github.com>
2022-11-25 23:54:50 +0900

    포스트 목록 페이지에 카테고리 기능 추가하기

--- blog/models.py
+++ blog/models.py
@@ -4,6 +4,13 @@ import os
 
 # Create your models here.
 
+class Category(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    slug = models.SlugField(max_length=200, unique=True, allow_unicode=True)
+    def __str__(self):
+        return self.name
+    class Meta:
+        verbose_name_plural = 'Categories'
 class Post(models.Model):
     title = models.CharField(max_length=30)
     hook_text = models.CharField(max_length=100, blank=True)
@@ -17,6 +24,7 @@ class Post(models.Model):
 
     author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
 
+    category = models.ForeignKey(Category, null=True, blank=True, on_delete=models.SET_NULL)
     def __str__(self):
         return f'[{self.pk}]{self.title} :: {self.author}'        # 해당 포스트의 pk 값, 해당 포스트의 title 값
 
@@ -28,3 +36,5 @@ class Post(models.Model):
 
     def get_file_ext(self):
         return self.get_file_name().split('.')[-1]
+
+
